717d9c056e69ee26c51f3978eca05f533b20fecc
Сергей <sergey.vasilev.98@mail.ru>
2024-08-25 15:07:01 +0300

    adding docstring

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs
 
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
 
+    """
+    Класс для создания веб-приложения с разными товарами, формой регистрации и FAQ
+    """
     def open_indexhtml(self):
+        """
+        Функция для открытия файла
+        :return:
+        """
         with open('index.html', encoding='utf-8') as file:
             text = file.read()
         return text
 
     def do_GET(self):
-        query_components = parse_qs(urlparse(self.path).query)
+        """
+        Функция для get-запроса
+        :return:
+        """
         page_content = self.open_indexhtml()
         self.send_response(200)
         self.send_header("Content_type", "text/html")
